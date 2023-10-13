73d54640639395c6be3fefe0903e7d214f856925
PC <saleh.majeed8@gmail.com>
2023-10-13 16:22:50 +0530

    added interval for debuggin instead of onFrame

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const canvas = document.getElementById('my-canvas');
-    paper.setup(canvas);
+var path = null;
+
+const canvas = document.getElementById('my-canvas');
+paper.setup(canvas);
+const { Path } = paper;
+
+path = new Path.Rectangle({
+  point: [75, 75],
+  size: [75, 75],
+  strokeColor: 'black',
 });
+
+console.log(path);
+
+setInterval(() => {
+  if (path === null) return;
+  path.rotate(3);
+  path.position.x += 1
+}, 10);
